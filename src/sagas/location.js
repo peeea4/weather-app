aa0bfd73ed0767fa565ec36904d89d7a3c9f8ba9
peeea4 <peeea4@gmail.com>
2022-08-22 19:02:55 +0300

    error handling

--- src/sagas/location.js
+++ src/sagas/location.js
@@ -9,34 +9,37 @@ import { setWeather, setWeatherStorm } from "@/actions/weather";
 import { GET_LOCATION, SET_CURRENT_LOCATION } from "@/constants/actions";
 import { fetchWeatherFromApi, fetchWeatherFromApiStorm } from "@/sagas/weather";
 
-export const fetchCoordsFromApi = (cityName) => axios.get(
-    `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=c75a2e7ac927f44818a689ec31f36fd5`,
-);
+export const fetchCoordsFromApi = async (cityName) => {
+    try {
+        const res = await axios.get(
+            `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=c75a2e7ac927f44818a689ec31f36fd5`,
+        );
+        return res;
+    } catch (error) {
+        console.log(error.response);
+        return error.response;
+    }
+};
 
 export function* fetchLocationWorker({ payload: cityName }) {
-    try {
-        yield put(setLoaderStatusAction(true));
-        const res = yield call(fetchCoordsFromApi, cityName);
+    yield put(setLoaderStatusAction(true));
+    const res = yield call(fetchCoordsFromApi, cityName);
+    if (!res.data.status) {
         const { data } = yield call(fetchWeatherFromApi, {
             latitude: res.data[0].lat,
             longitude: res.data[0].lon,
         });
         yield put(setWeather({ cityName, data }));
-        try {
-            const { dataStorm } = yield call(fetchWeatherFromApiStorm, {
-                latitude: res.data[0].lat,
-                longitude: res.data[0].lon,
-            });
-            yield put(setWeatherStorm({ cityName, dataStorm }));
-        } catch (error) {
-            console.log(error);
-        } finally {
-            yield put(setCurrentLocation(cityName));
-            yield delay(700);
-            yield put(setLoaderStatusAction(false));
-        }
-    } catch {
+        const { dataStorm } = yield call(fetchWeatherFromApiStorm, {
+            latitude: res.data[0].lat,
+            longitude: res.data[0].lon,
+        });
+        yield put(setWeatherStorm({ cityName, dataStorm }));
+
+        yield put(setCurrentLocation(cityName));
+        yield delay(700);
         yield put(setLoaderStatusAction(false));
+    } else {
         yield put(setCurrentLocation("Nothing found"));
     }
 }
--- src/sagas/weather.js
+++ src/sagas/weather.js
@@ -12,28 +12,36 @@ export const fetchWeatherFromApi = ({ latitude, longitude }) => axios.get(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${"c75a2e7ac927f44818a689ec31f36fd5"}&units=metric`,
 );
 
-export const fetchWeatherFromApiStorm = ({ latitude, longitude }) => axios.get(
-    `https://api.stormglass.io/v2/weather/point?lat=${latitude}&lng=${longitude}&params=${params}`,
-    {
-        headers: {
-            Authorization:
-                    "8d10ca84-1a32-11ed-a3aa-0242ac130002-8d10cafc-1a32-11ed-a3aa-0242ac130002",
-        },
-    },
-);
+export const fetchWeatherFromApiStorm = async ({ latitude, longitude }) => {
+    try {
+        const res = await axios.get(
+            `https://api.stormglass.io/v2/weather/point?lat=${latitude}&lng=${longitude}&params=${params}`,
+            {
+                headers: {
+                    Authorization:
+                        "8d10ca84-1a32-11ed-a3aa-0242ac130002-8d10cafc-1a32-11ed-a3aa-0242ac130002",
+                },
+            },
+        );
+        const { data } = res;
+        return data;
+    } catch (error) {
+        console.log(error.response);
+        return error.response;
+    }
+};
 export function* fetchWeatherWorker({ payload: coords }) {
     yield put(setLoaderStatusAction(true));
     const { data } = yield call(fetchWeatherFromApi, coords);
     yield put(setWeather({ cityName: data?.city?.name, data }));
-    try {
-        const dataStorm = yield call(fetchWeatherFromApiStorm, coords);
+    const dataStorm = yield call(fetchWeatherFromApiStorm, coords);
+    if (dataStorm.data) {
         yield put(setWeatherStorm({ cityName: data?.city?.name, data: dataStorm.data }));
-    } catch (error) {
-        alert("Try to use Open Weather Api");
+        yield put(setLoaderStatusAction(false));
+        yield put(setCurrentLocation(data?.city?.name));
+    } else {
         yield put(setLoaderStatusAction(false));
     }
-    yield put(setCurrentLocation(data?.city?.name));
-    yield put(setLoaderStatusAction(false));
 }
 
 export function* fetchWeatherWatcher() {
